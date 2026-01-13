732c490ba1a3d25876d772d2e6d0db59106e0997
Benjamin23k <wilsonbenjaminrosa@gmail.com>
2026-01-13 19:39:37 -0400

    Update Bot_telegram.py

--- Python/Bots/Bot_telegram/Bot_telegram.py
+++ Python/Bots/Bot_telegram/Bot_telegram.py
@@ -12,7 +12,7 @@ from telegram.ext import (
 )
 
 # ================== CONFIG ==================
-TOKEN = "7847516394:AAEy-AtqfWURynEI76PtwqN0-ByrKPdfo3g"  # ⚠️ Cambia este token, el anterior quedó público
+TOKEN = ""  # ⚠️ Cambia este token, el anterior quedó público
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_FILE = os.path.join(BASE_DIR, "emails.json")
 API_BASE = "https://api.mail.tm"
@@ -416,3 +416,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
